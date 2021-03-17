d90fe88e59b5cb7a78d2d825753531d49429f9cc
bunbalhara <nanjingzhe425@gmail.com>
2021-03-17 21:45:43 +0800

    commit

--- src/screens/admin/AddDealScreen.js
+++ src/screens/admin/AddDealScreen.js
@@ -1,16 +1,18 @@
 import React, {useState} from 'react';
-import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {View, StyleSheet, TouchableOpacity, Image} from 'react-native';
 import {useTheme} from 'react-native-paper';
-import {Button, Input, ProgressImage} from "../../components";
+import {Button, Input } from "../../components";
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import * as ImagePicker from 'expo-image-picker';
 import {useFirebase, useFirestore} from "react-redux-firebase";
 import uuid from 'react-native-uuid';
 import Spinner from "react-native-loading-spinner-overlay";
+import {validate} from "../../commons/helper";
+import {showMessage} from 'react-native-flash-message';
 
 const INITIAL_SATE = {
-    title: null,
-    image: null,
+    title: '',
+    image: '',
 };
 
 export const AddDealScreen = () => {
@@ -21,8 +23,17 @@ export const AddDealScreen = () => {
     const [deal, setDeal] = useState(INITIAL_SATE);
     const [progress, setProgress] = useState(0)
     const [loading, setLoading] = useState(false);
-    const submit = () => {
     
+    const submit = () => {
+        if(validate(deal,{title:'required', image: 'required'})){
+            firestore.collection('deals').add(deal).then(()=>{
+                showMessage({
+                    message: 'Success',
+                    description: 'Deal created successfully!',
+                    type: 'success',
+                });
+            })
+        }
     }
     
     const openImagePickerAsync = async () => {
@@ -35,7 +46,6 @@ export const AddDealScreen = () => {
         ImagePicker.launchImageLibraryAsync().then(async (res)=>{
             if(!res.cancelled){
                 const {uri} = res;
-                setDeal({...deal, image: uri});
                 const response = await fetch(uri);
                 const blob = await response.blob();
                 const fileName = uuid.v4() + '.' + uri.split('.').pop();
@@ -44,7 +54,7 @@ export const AddDealScreen = () => {
                     .put(blob).on(
                     "state_changed",
                     (snapshot )=> {
-                        setProgress(snapshot.bytesTransferred/snapshot.totalBytes);
+                        setProgress(Math.floor(snapshot.bytesTransferred/snapshot.totalBytes * 100));
                     },
                     (error) => {
                         console.log(error);
@@ -55,6 +65,7 @@ export const AddDealScreen = () => {
                             .child(fileName)
                             .getDownloadURL()
                             .then((url) => {
+                                setDeal({...deal, image: url});
                                 setProgress(0);
                                 setLoading(false);
                             })
@@ -78,7 +89,7 @@ export const AddDealScreen = () => {
                 {
                     deal.image?
                         <View style={styles.imagePicker}>
-                            <ProgressImage source={{uri: deal.image}}/>
+                            <Image source={{uri: deal.image}}/>
                         </View>:
                         <TouchableOpacity style={styles.imagePicker} onPress={openImagePickerAsync}>
                             <AntDesign name={'camera'} size={theme.wp('20%')} color={'white'} />

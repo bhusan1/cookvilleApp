a672161a136c1ca0794f954e29df04291dba2a89
bunbalhara <nanjingzhe425@gmail.com>
2021-03-18 09:16:52 +0800

    commit

--- src/screens/user/HomeScreen.js
+++ src/screens/user/HomeScreen.js
@@ -3,7 +3,6 @@ import {View, Text, StyleSheet, Alert, Linking, ScrollView, SafeAreaView, Status
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import {Card} from 'react-native-elements';
 import MarqueeText from 'react-native-marquee';
-import Icon from 'react-native-vector-icons/Ionicons';
 import Constants from 'expo-constants';
 import {useSelector} from 'react-redux';
 import * as Notifications from 'expo-notifications';
@@ -40,7 +39,7 @@ const OpenURLButton = ({url, children}) => {
             Alert.alert(`Don't know how to open this URL: ${url}`);
         }
     }, [url]);
-    return <Button title={children} onPress={handlePress} />;
+    return <Button style={{padding: 2,}} titleStyle={{fontFamily:'OpenSans'}} title={children} onPress={handlePress} />;
 };
 
 export const HomeScreen = () => {
@@ -56,7 +55,7 @@ export const HomeScreen = () => {
     
     const [refresh, setRefresh] = useState(false);
     const [region] = useState({
-        latitude: 33.18624068627443,
+        latitude: 33.18454068627443,
         longitude: -94.86102794051021,
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
@@ -81,61 +80,62 @@ export const HomeScreen = () => {
     
     return (
         <SafeAreaView style={styles.root}>
-            <ScrollView>
-                <View style={styles.saCon}>
-                    <Text style={styles.gasText}>Store Address {'\n'} Cookville #1 Stop</Text>
-                    <Text style={styles.dealsText}> 6262 US HWY 67 E Cookville, TX 75558 </Text>
+            <View style={styles.mapFix} pointerEvents={'box-none'}>
+                <View style={styles.topPanel}>
+                    <View style={styles.topPanelContent}>
+                        <View style={styles.saCon}>
+                            <Text style={styles.gasText}>Store Address {'\n'} Cookville #1 Stop</Text>
+                        </View>
+                        <View style={styles.divider}>
+                            <MarqueeText
+                                style={{fontSize: 18, color: '#bc245c', paddingBottom: 10,}}
+                                duration={6000}
+                                marqueeOnStart
+                                loop={true}
+                                marqueeDelay={1000}
+                                marqueeResetDelay={500}>
+                                Find Deals on In-store Purchase and Deli and Save on Gas
+                            </MarqueeText>
+                        </View>
+                    </View>
+                </View>
+                <Paper onPress={resetMap} style={styles.getCurrentLocation}>
+                    <Feather name={'send'} size={20} />
+                </Paper>
+                <Paper style={styles.address}>
+                    <Text style={styles.dealsText}>6262 US HWY 67 E Cookville, TX 75558</Text>
                     <OpenURLButton url={url}>Open Maps</OpenURLButton>
-                </View>
-                <View style={styles.mapFix}>
-                    <Paper onPress={resetMap} style={styles.getCurrentLocation}>
-                        <Feather name={'send'} size={20} />
-                    </Paper>
-                    <MapView
-                        key={refresh}
-                        style={styles.mapFix}
-                        provider={PROVIDER_GOOGLE}
-                        region={region}
-                    >
-                        <Marker coordinate={{latitude: 33.18624068627443, longitude: -94.86102794051021}} />
-                    </MapView>
-                </View>
-                <View style={styles.divider}>
-                    <MarqueeText
-                        style={{fontSize: 24, color: '#bc245c'}}
-                        duration={6000}
-                        marqueeOnStart
-                        loop={true}
-                        marqueeDelay={1000}
-                        marqueeResetDelay={500}>
-                        Find Deals on In-store Purchase and Deli and Save on Gas
-                    </MarqueeText>
-                </View>
-                <View style={styles.gpCon}>
-                    <Text style={styles.gasText}>Gas Price</Text>
-                    <FontAwesome5 name="gas-pump" color="black" size={70} style={{marginVertical: 10}} />
-                    <Text style={styles.dateText}>{moment().format('MM/DD/YYYY')}</Text>
-                </View>
-                <View style={styles.priceContainer}>
-                    <View style={styles.col1}>
-                        <Card size="15" title="Regular">
-                            <Text style={styles.paragraph}>REGULAR {'\n'} {gasPrice.regular || 0}/gal</Text>
-                        </Card>
-                        <Card size="12" title="Plus">
-                            <Text style={styles.paragraph}>PLUS {'\n'} {gasPrice.plus || 0}/gal</Text>
-                        </Card>
+                </Paper>
+                <MapView
+                    key={refresh}
+                    style={styles.mapFix}
+                    provider={PROVIDER_GOOGLE}
+                    region={region}
+                >
+                    <Marker coordinate={{latitude: 33.18624068627443, longitude: -94.86102794051021}} />
+                </MapView>
+                <View style={styles.footerPanel}>
+                    <View style={styles.gpCon}>
+                        <Text style={styles.gasText}>Gas Price</Text>
+                        <FontAwesome5 name="gas-pump" color="black" size={50} style={{marginVertical: 2}} />
+                        <Text style={styles.dateText}>{moment().format('MM/DD/YYYY')}</Text>
                     </View>
-
-                    <View style={styles.col1}>
-                        <Card title="Super">
-                            <Text style={styles.paragraph}>SUPER {'\n'} {gasPrice.super || 0}/gal</Text>
-                        </Card>
-                        <Card title="Diesel">
-                            <Text style={styles.paragraph}>DIESEL {'\n'}{gasPrice.diesel || 0}/gal</Text>
-                        </Card>
+                    <View style={styles.priceContainer}>
+                        <View style={styles.priceItem}>
+                            <Text style={styles.paragraph}>REGULAR{'\n'}{gasPrice.regular || 0}/gal</Text>
+                        </View>
+                        <View style={styles.priceItem}>
+                            <Text style={styles.paragraph}>PLUS{'\n'}{gasPrice.plus || 0}/gal</Text>
+                        </View>
+                        <View style={styles.priceItem}>
+                            <Text style={styles.paragraph}>SUPER{'\n'}{gasPrice.super || 0}/gal</Text>
+                        </View>
+                        <View  style={styles.priceItem}>
+                            <Text style={styles.paragraph}>DIESEL{'\n'}{gasPrice.diesel || 0}/gal</Text>
+                        </View>
                     </View>
                 </View>
-            </ScrollView>
+            </View>
         </SafeAreaView>
     );
 };
@@ -143,27 +143,54 @@ export const HomeScreen = () => {
 const useStyles = theme => StyleSheet.create({
     root: {
         flex: 1,
+        width:'100%',
         marginTop: StatusBar.currentHeight,
+        zIndex: 0,
     },
     container: {
         justifyContent: 'center',
     },
-    topImg: {
-        paddingTop: 20,
-        width: 300,
-        height: 200,
+    topPanel: {
+        position:'absolute',
+        top: 0,
+        left: 0,
+        width:'100%',
+        zIndex: 2,
+        paddingBottom: 5,
+        overflow: 'hidden'
+    },
+    topPanelContent:{
+        ...theme.styles.shadow,
+        borderBottomRightRadius: theme.wp('5%'),
+        borderBottomLeftRadius: theme.wp('5%'),
+        overflow:'hidden',
+    },
+    
+    footerPanel:{
+        position:'absolute',
+        bottom: 0,
+        width: '100%',
+        ...theme.styles.shadow,
     },
     mapFix: {
-        width: 400,
-        height: 300,
+        width: theme.wp('100%'),
+        height: '100%',
         position:'relative',
         flex: 1,
+    },
+    address:{
+        opacity: 0.9,
+        position:'absolute',
+        left: 5,
+        top: 140,
+        flexDirection:'column',
+        width: 150,
     },
     getCurrentLocation:{
         width: 40,
         height: 40,
         position: 'absolute',
-        top: theme.hp('2%'),
+        top: 140,
         right: theme.wp('5%'),
         justifyContent: 'center',
         alignItems: 'center',
@@ -171,7 +198,7 @@ const useStyles = theme => StyleSheet.create({
         zIndex: 2,
     },
     divider: {
-        padding: 20,
+        padding: 10,
         backgroundColor: 'white',
     },
     gasText: {
@@ -179,15 +206,13 @@ const useStyles = theme => StyleSheet.create({
         fontWeight: 'bold',
         textAlign: 'center',
         color: '#bc245c',
-        paddingTop: 20,
+        paddingTop: 5,
     },
     dealsText: {
-        fontSize: 15,
+        fontSize: 12,
         color: '#bc245c',
         fontWeight: 'bold',
-        textAlign: 'center',
-        paddingTop: 20,
-        paddingBottom: 20,
+        textAlign:'center',
     },
     dateText: {
         fontSize: 15,
@@ -201,7 +226,7 @@ const useStyles = theme => StyleSheet.create({
         height: 50,
     },
     saCon: {
-        padding: 20,
+        paddingTop: 20,
         backgroundColor: '#fff',
     },
     gpCon: {
@@ -211,67 +236,23 @@ const useStyles = theme => StyleSheet.create({
     priceContainer: {
         backgroundColor: '#fff',
         alignItems: 'center',
+        flexDirection:'row',
+        justifyContent:'center',
+        paddingBottom: 10,
     },
     paragraph: {
-        fontSize: 16,
+        fontSize: 14,
         fontWeight: 'bold',
         textAlign: 'center',
         color: '#bc245c',
     },
-
-    col1: {
-        flexDirection: 'row',
-        height: 100,
-        alignItems: 'center',
-    },
-    formContainer: {
-        flexDirection: 'row',
-        height: 80,
-        marginTop: 40,
-        marginBottom: 20,
-        flex: 1,
-        paddingTop: 10,
-        paddingBottom: 10,
-        paddingLeft: 30,
-        paddingRight: 30,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    input: {
-        height: 48,
-        borderRadius: 5,
-        overflow: 'hidden',
-        backgroundColor: 'white',
-        paddingLeft: 16,
-        flex: 1,
-        marginRight: 5,
-    },
-    button: {
-        height: 47,
-        borderRadius: 5,
-        backgroundColor: '#788eec',
-        width: 80,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    buttonText: {
-        color: 'white',
-        fontSize: 16,
-    },
-    listContainer: {
-        marginTop: 20,
-        padding: 20,
-    },
-    entityContainer: {
-        marginTop: 16,
-        borderBottomColor: '#cccccc',
-        borderBottomWidth: 1,
-        paddingBottom: 16,
-    },
-    entityText: {
-        fontSize: 20,
-        color: '#333333',
-    },
+    priceItem:{
+        width: '20%',
+        margin:'1%',
+        padding: 5,
+        borderStyle:'solid',
+        borderWidth: 0.5,
+    }
 });
 
 

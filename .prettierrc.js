106733e22726eedca69f87ce729f30ac50302496
bunbalhara <nanjingzhe425@gmail.com>
2021-03-21 01:18:47 +0800

    commit

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  printWidth: 120,
-  tabWidth: 4,
-  detectIndentation: false
-};
